16e489b9dccbdfcda00ddbe2a794a7e715fa0d17
Kunal verma <kunal33grd@gmail.com>
2025-09-05 11:36:46 +0530

    resolving conflict of NA project pictures

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,6 +3,8 @@ import project1 from '../assets/project1.png';
 import project2 from '../assets/project2.png';
 import project3 from '../assets/project3.png';
 import project4 from '../assets/project4.png';
+import project5 from '../assets/project5.png';
+import project6 from '../assets/project6.png';
 import '../App.css'; 
 
 const Projects = () => {
